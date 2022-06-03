06b4845591fdf8df0914228ae8d8277514bd1af0
luanvuunee <luanvuforwork@gmail.com>
2022-06-04 01:31:56 +0700

    gan navlink ui dashboard student

--- src/components/Common/index.ts
+++ src/components/Common/index.ts
@@ -1,3 +1,10 @@
 export * from './NotFound'
 export * from './PrivateRoute'
 export * from './Notifications'
+export * from './Header'
+export * from './Main'
+export * from './Sidebar'
+
+
+
+
--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,25 +1,25 @@
 
 
-import { Box, Button, Paper, Typography } from '@mui/material';
-import { useAppDispatch } from 'app/hooks';
+import { Box, Button, CircularProgress, Paper, Typography } from '@mui/material';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import Notifications from 'components/Common/Notifications';
-import { authAction } from '../authSlice';
+import { useSelector } from 'react-redux';
+import { authAction, selectedIsLogging } from '../authSlice';
 
-export interface LoginPageProps {}
+export interface LoginPageProps { }
 
 export default function LoginPage(props: LoginPageProps) {
 
   const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(selectedIsLogging)
   const handleLogin = () => {
     dispatch(authAction.login({
-      username :'admin', password :'admin',
+      username: 'admin', password: 'admin',
     }
-      
+
     ))
   }
-  const handleLogout = () => {
-    dispatch(authAction.logOut())
-  }
+
   return (
     <Box
       sx={{
@@ -30,23 +30,23 @@ export default function LoginPage(props: LoginPageProps) {
         minHeight: '100vh',
       }}
     >
-      
-        
-    
-      <Paper elevation={1} sx={{p:'1rem' ,width:'20vw',"& .MuiTypography-root":{
-        textAlign:"center"
-      }}}>
+      <Paper elevation={1} sx={{
+        p: '1rem', width: '20vw', "& .MuiTypography-root": {
+          textAlign: "center"
+        }
+      }}>
         <Typography variant="h5" component="h1" >
           Quản lý học sinh
         </Typography>
         <Box mt={4} >
-          <Button variant="contained" fullWidth color="primary" onClick={handleLogin} sx={{mb:"4px"}}>
+          <Button variant="contained" fullWidth color="primary" onClick={handleLogin} sx={{ mb: "4px" }}>
+
+            {isLoggedIn && <CircularProgress size={20} color="warning" />}
+            &nbsp;
             Login Fake
           </Button>
 
-          <Button variant="contained" fullWidth color="primary" onClick={handleLogout}>
-            Logout Fake
-          </Button>
+
         </Box>
       </Paper>
     </Box>
